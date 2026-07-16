96e66a17c6ff4b6e89bfb3b39e62a96bd76ab872
iukim21c <iukim21c@gmail.com>
2026-07-17 03:09:54 +0900

    Debug: n_threads=1 단일스레드 + tdrz_enable=false - 멀티스레드 데드락/tdrz 버그 격리 테스트

--- app/src/main/cpp/whisper_jni.cpp
+++ app/src/main/cpp/whisper_jni.cpp
@@ -95,11 +95,12 @@ Java_com_grepiu_aidiary_data_slm_WhisperEngine_nativeTranscribe(
 
     struct whisper_full_params params = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
     params.language         = lang;
-    params.tdrz_enable      = true;
-    params.print_progress   = true;
+    params.n_threads        = 1;
+    params.print_progress   = false;
     params.print_realtime   = false;
     params.print_timestamps = false;
     params.no_context       = true;
+    params.tdrz_enable      = false;
 
     int ret = whisper_full(ctx, params, samples.data(), static_cast<int>(samples.size()));
     LOGD("nativeTranscribe: whisper_full returned %d", ret);
